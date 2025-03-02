fd2dc852f3a3aa8a1700594d1edd72f2dab9bbf1
Carlo Wood <carlo@alinoe.com>
2025-03-02 19:06:18 +0100

    Add wf::rectangle_t, wf::geometry_difference_t and manipulators.
    Most manipulators are free functions, which I prefer for binary
    operators anyway (which then normally would be a friend, but
    that is not necessary here because all members are public).
    
    wf::rectangle_t is another way to represent a geometry_t, but
    allowing for much easier and cleaner manipulation.
    
    wf::geometry_difference_t is literally decoration_margins_t, but
    independent of decorations (or margins); it is a type on its own
    that represents the difference between two geometry_t's.
    
    Provided manipulators:
    Adding and subtracting a geometry_difference_t to/from a geometry_t.
    Subtracting two geometry_t.
    Negating a geometry_difference_t.
    Conditionally add geometry_difference_t to geometry_t.
    Conditionally pick one of two geometry_t.
    Conditionally subtract two geometry_difference_t.
    
    All the conditional functions take a `int tiled_edges` parameter
    and then act on each edge as function of whether or not the bit
    in tiled_edges for that edge is set.
    
    These classes and functions are required to implement unidirectional
    maximization, but rather support manipulation on a per-edge basis,
    as opposed to always two opposing edges at the same time.

--- src/api/wayfire/geometry.hpp
+++ src/api/wayfire/geometry.hpp
@@ -69,6 +69,29 @@ struct dimensions_t
 
 using geometry_t = wlr_box;
 
+struct rectangle_t
+{
+    int x1, y1; // Top-left coorinate, inclusive.
+    int x2, y2; // One pixel beyond the bottom-right (aka, not inclusive).
+
+    // Default constructor.
+    rectangle_t() = default;
+
+    // Construct a rectangle_t from top-left and bottom-right coordinates.
+    rectangle_t(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(x2), y2(y2)
+    {}
+
+    // Construct a rectangle_t from a geometry_t.
+    rectangle_t(geometry_t g) : x1(g.x), y1(g.y), x2(g.x + g.width), y2(g.y + g.height)
+    {}
+
+    // We can't add a constructor to geometry_t because that must be an aggregate type.
+    // Therefore add conversion from rectangle_t to geometry_t here.
+    operator geometry_t() const {
+        return {x1, y1, x2 - x1, y2 - y1};
+    }
+};
+
 point_t origin(const geometry_t& geometry);
 dimensions_t dimensions(const geometry_t& geometry);
 geometry_t construct_box(
@@ -111,7 +134,137 @@ geometry_t clamp(geometry_t window, geometry_t output);
 // The returned subbox will occupy the same relative part of @B as
 // @box occupies in @A.
 wf::geometry_t scale_box(wf::geometry_t A, wf::geometry_t B, wf::geometry_t box);
+
+/**
+ *
+ * Let the members of geometry_difference_t be an outwards displacement.
+ *
+ *                   from.x
+ *                       |
+ *              to.x     v
+ *                |      +<-----from.width----->+
+ *                v      |                      |
+ *      to.y ->---┌─────────────────────────────────────┐--------------+
+ *                │      |         ^ d.top      |       │              ^
+ *    from.y ->---│------┌─────────┴────────────┐-------│----+         |
+ *                │      │                      │       │    ^         |
+ *                │d.left│                      │d.right│    |         |
+ *                │<-----│                      │------>│  from.height |
+ *                │      │                      │       │    |         |
+ *                │      │                      │       │    v     to.height
+ *                │      └─────────┬────────────┘-------│----+         |
+ *                │                v d.bottom           │              v
+ *                └─────────────────────────────────────┘--------------+
+ *                |                                     |
+ *                +<-------------to.width-------------->+
+ *
+ * geometry_t from;
+ * geometry_difference_t d;
+ * geometry_t to = from + d;
+ *
+ * In the above `from` is expanded, grown, in each of the four directions.
+ * However, negative values of left, right, bottom and top are also legal;
+ * in that case the corresponding border simply shifts the opposite way.
+ *
+ * It is not possible to add geometry_t's or to negate them, of course.
+ * But it is possible to subtract them: d = to - from.
+ */
+struct geometry_difference_t
+{
+    int left;
+    int right;
+    int bottom;
+    int top;
+
+    geometry_difference_t& subtract_if(uint32_t tiled_edges, geometry_difference_t delta);
+};
+
+// Allow printing a geometry_difference_t.
+std::ostream& operator <<(std::ostream& stream, geometry_difference_t const& geometry_difference);
+
+/**
+ * Add a geometry_difference_t to a geometry_t.
+ */
+inline geometry_t operator +(geometry_t const& g, geometry_difference_t const& m)
+{
+    return {g.x - m.left, g.y - m.top, g.width + m.left + m.right, g.height + m.top + m.bottom};
 }
+
+/**
+ * Subtract a geometry_difference_t from a geometry_t.
+ */
+inline geometry_t operator -(geometry_t const& g, geometry_difference_t const& m)
+{
+    return {g.x + m.left, g.y + m.top, g.width - m.left - m.right, g.height - m.top - m.bottom};
+}
+
+/**
+ * Subtract a geometry_t from a geometry_t.
+ */
+inline geometry_difference_t operator -(geometry_t const& to, geometry_t const& from)
+{
+    return {.left = from.x - to.x, .right = to.x + to.width - (from.x + from.width),
+        .bottom   = to.y + to.height - (from.y + from.height), .top = from.y - to.y};
+}
+
+/**
+ * Conditionally expand (or shrink in case of negative values) each edge with
+ * the displacement given by delta OR tiled_edge_delta, as a function of `tiled_edges`.
+ *
+ * If the corresponding WLR_EDGE_* bit is set in `tiled_edges` then `tiled_edge_delta`
+ * is used, otherwise `delta` is used. The order of the arguments has been chosen
+ * to match `tiled_edges ? tiled_edge_delta : delta`.
+ */
+inline geometry_t expand_geometry_if(geometry_t geometry_in,
+    uint32_t tiled_edges,
+    geometry_difference_t const& tiled_edge_delta,
+    geometry_difference_t const& delta)
+{
+    rectangle_t rectangle{geometry_in};
+
+    rectangle.x1 -= (tiled_edges & WLR_EDGE_LEFT) ? tiled_edge_delta.left : delta.left;
+    rectangle.y1 -= (tiled_edges & WLR_EDGE_TOP) ? tiled_edge_delta.top : delta.top;
+    rectangle.x2 += (tiled_edges & WLR_EDGE_RIGHT) ? tiled_edge_delta.right : delta.right;
+    rectangle.y2 += (tiled_edges & WLR_EDGE_BOTTOM) ? tiled_edge_delta.bottom : delta.bottom;
+
+    return rectangle;
+}
+
+/**
+ * Return a geometry with edges from tiled_edge_rect or rect respectively, as function of tiled_edges.
+ */
+inline rectangle_t geometry_switch_if(uint32_t tiled_edges, rectangle_t const& tiled_edge_rect,
+    rectangle_t const& rect)
+{
+    return {
+        (tiled_edges & WLR_EDGE_LEFT) ? tiled_edge_rect.x1 : rect.x1,
+        (tiled_edges & WLR_EDGE_TOP) ? tiled_edge_rect.y1 : rect.y1,
+        (tiled_edges & WLR_EDGE_RIGHT) ? tiled_edge_rect.x2 : rect.x2,
+        (tiled_edges & WLR_EDGE_BOTTOM) ? tiled_edge_rect.y2 : rect.y2
+    };
+}
+
+/**
+ * Conditionally subtract another geometry_difference_t, as function of tiled_edges.
+ */
+inline geometry_difference_t& geometry_difference_t::subtract_if(uint32_t tiled_edges,
+    geometry_difference_t delta)
+{
+    left   -= (tiled_edges & WLR_EDGE_LEFT) ? static_cast<int>(delta.left) : 0;
+    right  -= (tiled_edges & WLR_EDGE_RIGHT) ? static_cast<int>(delta.right) : 0;
+    bottom -= (tiled_edges & WLR_EDGE_BOTTOM) ? static_cast<int>(delta.bottom) : 0;
+    top    -= (tiled_edges & WLR_EDGE_TOP) ? static_cast<int>(delta.top) : 0;
+    return *this;
+}
+
+/**
+ * Invert all displacements of a geometry_difference_t.
+ */
+inline geometry_difference_t operator -(geometry_difference_t const& delta)
+{
+    return {.left = -delta.left, .right = -delta.right, .bottom = -delta.bottom, .top = -delta.top};
+}
+} // namespace wf
 
 bool operator ==(const wf::geometry_t& a, const wf::geometry_t& b);
 bool operator !=(const wf::geometry_t& a, const wf::geometry_t& b);
@@ -133,7 +286,7 @@ bool operator &(const wf::geometry_t& rect, const wf::pointf_t& point);
 /* Returns true if the two geometries have a common point */
 bool operator &(const wf::geometry_t& r1, const wf::geometry_t& r2);
 
-/* Make geometry and point printable */
+/* Make geometry_t printable */
 std::ostream& operator <<(std::ostream& stream, const wf::geometry_t& geometry);
 
 #endif /* end of include guard: WF_GEOMETRY_HPP */
